03d4d4436bcc407719a0a745fe41558d18ca2476
Atanas Bozhkov <bozhkov.atanas@gmail.com>
2015-03-19 03:00:17 +0000

    Added basic setup + bootsrap

--- TWD/urls.py
+++ TWD/urls.py
@@ -1,10 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from TWD import views
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'TWD.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+        url(r'^$', views.index, name='index'))
 
-    url(r'^admin/', include(admin.site.urls)),
-)
