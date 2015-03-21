5f73de31ed9892af49fb21fc4c2a2dee1c94f3e9
Kelvin Ho <ckhokelvin+git@gmail.com>
2015-03-21 22:14:37 +0000

    fixed the merge; two applications exist at the same time, need to be merged

--- TWD/urls.py
+++ TWD/urls.py
@@ -1,13 +1,16 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from TWD import views
 from django.conf import settings
+from burger import views
 
 urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'TWD.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
     url(r'^$', views.index, name='index'),
-    url(r'^burger/', include('burger.urls')),
     url(r'^admin/', include(admin.site.urls)),
-
+    url(r'^KC/', include('KC.urls')),
+    url(r'^burger/', include('burger.urls')),
 )
 
 if settings.DEBUG:
