699b56da69f3c5d8602e4c4cafe65401740e234d
Kelvin Ho <ckhokelvin+git@gmail.com>
2015-03-21 17:02:57 +0000

    gone through tutorial 7

--- TWD/urls.py
+++ TWD/urls.py
@@ -1,9 +1,18 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from TWD import views
+from django.conf import settings
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^burger/', include('burger.urls'))
+    url(r'^burger/', include('burger.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+
 )
 
+if settings.DEBUG:
+    urlpatterns += patterns(
+        'django.views.static',
+        (r'^media/(?P<path>.*)',
+        'serve',
+        {'document_root': settings.MEDIA_ROOT}), )
--- burger/urls.py
+++ burger/urls.py
@@ -2,4 +2,7 @@ from django.conf.urls import patterns, url
 from burger import views
 
 urlpatterns = patterns('',
-        url(r'^$', views.index, name='index'))
+    url(r'^$', views.index, name='index'),
+    url(r'^categoryList/', views.categoryList, name='categoryList'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category')
+)
