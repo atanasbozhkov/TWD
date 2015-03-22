fef0867e7423f5f266ff37e3feda1d6792328655
Kelvin Ho <ckhokelvin+git@gmail.com>
2015-03-22 00:22:39 +0000

    use django-registration-redux to handle authentication

--- TWD/urls.py
+++ TWD/urls.py
@@ -2,6 +2,12 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
 from burger import views
+from registration.backends.simple.views import RegistrationView
+
+# Create a new class that redirects the user to the index page, if successful at logging
+class MyRegistrationView(RegistrationView):
+    def get_success_url(self,request, user):
+        return '/burger/'
 
 urlpatterns = patterns('',
     # Examples:
@@ -10,6 +16,9 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^burger/', include('burger.urls')),
+    #Add in this url pattern to override the default pattern in accounts.
+    url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
+    (r'^accounts/', include('registration.backends.simple.urls')),
 )
 
 if settings.DEBUG:
